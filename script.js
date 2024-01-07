641dc2befaf996adfcfbaf89f9bdb3a79560466e
tmcnulty387 <tmcnulty387@gmail.com>
2024-01-07 14:50:07 -0500

    Started working on the functionality that initializes the chess board

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const boardSize = 8;
-let board = [];
-for(let row = 0; row < boardSize; row++){
-    board[row] = []
-    for(let col = 0; col < boardSize; col++)
+const boardSize = 8; // 8x8 chessboard
+const chessboard = document.getElementById('chessboard');
+
+// initializes chess board
+for (let row = 0; row < boardSize; row++) {
+    for (let col = 0; col < boardSize; col++) {
+        let square = document.createElement('div');
+        square.id = String.fromCharCode('a'.charCodeAt(0) + col) + (boardSize - row);
+        square.classList.add('chess-square');
+        chessboard.appendChild(square);
+    }
 }
